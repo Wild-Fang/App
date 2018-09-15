8258feddda60d768ca19a18598d0999d9a003f84
Arthur Guiot <arguiot@gmail.com>
2018-09-15 21:00:51 +0800

    Added CaptureVideo initialization

---
+++ Wild Fang/Controllers/Extensions/AVCapture.swift
@@ -0,0 +1,35 @@
+//
+//  AVCapture.swift
+//  Wild Fang
+//
+//  Created by Arthur Guiot on 15/9/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        connection.videoOrientation = .portrait
+        
+        var requestOptions:[VNImageOption: Any] = [:]
+        
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
+            requestOptions = [.cameraIntrinsics: cameraIntrinsicData]
+        }
+        
+        // for orientation see kCGImagePropertyOrientation
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+    }
+}
---
+++ Wild Fang/Controllers/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Wild Fang
+//
+//  Created by Arthur Guiot on 15/9/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController {
+    // Model
+    let model = SqueezeNet().model
+    
+    
+    // video capture session
+    let session = AVCaptureSession()
+    // preview layer
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    // queue for processing video frames
+    let captureQueue = DispatchQueue(label: "captureQueue")
+    // overlay layer
+    var gradientLayer: CAGradientLayer!
+    // vision request
+    var visionRequests = [VNRequest]()
+    var recognitionThreshold : Float = 0.25
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
