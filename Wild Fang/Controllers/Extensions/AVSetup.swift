ffb7a60258afe1ed3a1178145ae4a62b9784f9f9
Arthur Guiot <arguiot@gmail.com>
2018-09-15 21:01:09 +0800

    Added setup + test model

---
+++ Wild Fang/Controllers/Extensions/AVSetup.swift
@@ -0,0 +1,69 @@
+//
+//  AVSetup.swift
+//  Wild Fang
+//
+//  Created by Arthur Guiot on 15/9/18.
+//  Copyright © 2018 Arthur Guiot. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Vision
+
+extension ViewController {
+    func setup() {
+        
+        // get hold of the default video camera
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            fatalError("No video camera available")
+        }
+        
+        do {
+            
+            // add the preview layer
+            previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            self.previewView.layer.addSublayer(previewLayer)
+            // add a slight gradient overlay so we can read the results easily
+            gradientLayer = CAGradientLayer()
+            gradientLayer.colors = [
+                UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.7).cgColor,
+                UIColor.init(red: 0, green: 0, blue: 0, alpha: 0.0).cgColor,
+            ]
+            gradientLayer.locations = [0.0, 0.3]
+            self.previewView.layer.addSublayer(gradientLayer)
+            
+            // create the capture input and the video output
+            let cameraInput = try AVCaptureDeviceInput(device: camera)
+            
+            let videoOutput = AVCaptureVideoDataOutput()
+            videoOutput.setSampleBufferDelegate(self, queue: captureQueue)
+            videoOutput.alwaysDiscardsLateVideoFrames = true
+            videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA]
+            session.sessionPreset = .high
+            
+            // wire up the session
+            session.addInput(cameraInput)
+            session.addOutput(videoOutput)
+            
+            // make sure we are in portrait mode
+            let conn = videoOutput.connection(with: .video)
+            conn?.videoOrientation = .portrait
+            
+            // Start the session
+            session.startRunning()
+            
+            
+            // set up the vision model
+            guard let model = try? VNCoreMLModel(for: self.model) else {
+                fatalError("Could not load model")
+            }
+            // set up the request using our vision model
+            let classificationRequest = VNCoreMLRequest(model: model, completionHandler: handleClassification)
+            classificationRequest.imageCropAndScaleOption = .centerCrop
+            visionRequests = [classificationRequest]
+        } catch {
+            print("Error")
+        }
+        
+    }
+}
