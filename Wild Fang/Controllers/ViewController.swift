a4ed920403cdbd7e3a35349cd98bea33273950ed
Arthur Guiot <arguiot@gmail.com>
2018-09-20 13:00:53 +0800

    Added privacy message

--- Wild Fang/Controllers/ViewController.swift
+++ Wild Fang/Controllers/ViewController.swift
@@ -34,6 +34,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.setup()
         
     }
     
